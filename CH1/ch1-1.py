04abf5c5f0dd444ebee908e8e672c015ed962275
Hossein <hossein@Hosseins-MacBook-Air.local>
2016-06-15 07:44:57 -0400

    first

--- CH1/ch1-1.py
+++ CH1/ch1-1.py
@@ -1,1 +1,20 @@
 # Problem 1.1
+
+def has_all_unique(instr):
+	exists = [False]*256  #[False for i in range(256)]
+	for i in range(len(instr)):
+		#print "check ", instr[i]
+		if exists[ ord(instr[i]) ] == True:
+			return False
+		exists[ ord(instr[i]) ] = True
+	return True
+
+
+def test1():
+        tcases = ["tes", "test1", "  ", "''"]
+	for t in tcases:
+        	print t, has_all_unique(t)
+
+if __name__ == "__main__":
+	test1()
+
