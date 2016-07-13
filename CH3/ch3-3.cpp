891385ff949938b38f5fa1b322232f0be5d0f693
matxu <tiantianxu.88@gmail.com>
2016-07-12 21:02:27 -0400

    Create ch3-3.cpp

---
+++ CH3/ch3-3.cpp
@@ -0,0 +1,16 @@
+// Design a class: set of stacks
+
+/*
+Use LinkedList to implement stack, where top is the first node
+Set of stacks is a LinkedList, each node stores the pointer to one of the stacks
+Push: add one node to the stack pointed by first node. 
+If capacity of that stack is reached, create a new stack, and a new node that stores the pointer to the new stack.
+Pop: similarlly pop removes the first node of the first stack.
+
+*/
+
+/*Follow up:
+popAt(id): pop a specific stack
+Index to the id-th stack in linked list
+
+*/
